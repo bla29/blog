e135a2e060865246a30469b2863dd85fb7832992
Brian La <133820463+bla29@users.noreply.github.com>
2025-03-10 21:40:54 -0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get "hello_koyeb/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "hello_world#index"
-  #root "articles#index"
+  root "articles#index"
 
   resources :articles do 
     resources :comments
